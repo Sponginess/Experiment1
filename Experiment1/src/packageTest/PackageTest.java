90f9bbd921aec6c956e04231ad28c75fe1567a17
Sponginess <animerocksforever@hotmail.com>
2014-10-01 19:18:54 +0800

    Testing staging criteria

--- Experiment1/src/packageTest/PackageTest.java
+++ Experiment1/src/packageTest/PackageTest.java
@@ -1,5 +1,8 @@
 package packageTest;
 
 public class PackageTest {
-
+	public static void main(String[] args) {
+		System.out.println("More testing");
+		System.out.println("Testing criteria for staging");
+	}
 }
